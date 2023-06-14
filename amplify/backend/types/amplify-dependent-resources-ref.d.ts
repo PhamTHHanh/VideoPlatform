555cef8cb732c57cace7997d0b5c9c885ead97b1
PhamTHHanh <103091745+PhamTHHanh@users.noreply.github.com>
2023-06-14 10:37:58 +0700

    Merge file

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -28,71 +28,38 @@ export type AmplifyDependentResourcesAttributes = {
       "Region": "string"
     }
   },
+
   "storage": {
-    "Course": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
-    },
-<<<<<<< HEAD
-    "storage": {
-        "fcjvideoplatform": {
-            "BucketName": "string",
-            "Region": "string"
-        },
-        "Course": {
-            "Name": "string",
-            "Arn": "string",
-            "StreamArn": "string",
-            "PartitionKeyName": "string",
-            "PartitionKeyType": "string",
-            "Region": "string"
-        },
-        "Lectures": {
-            "Name": "string",
-            "Arn": "string",
-            "StreamArn": "string",
-            "PartitionKeyName": "string",
-            "PartitionKeyType": "string",
-            "SortKeyName": "string",
-            "SortKeyType": "string",
-            "Region": "string"
-        },
-        "Section": {
-            "Name": "string",
-            "Arn": "string",
-            "StreamArn": "string",
-            "PartitionKeyName": "string",
-            "PartitionKeyType": "string",
-            "SortKeyName": "string",
-            "SortKeyType": "string",
-            "Region": "string"
-        }
-=======
-    "CourseCategory": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
->>>>>>> b307d78aa8ef7439fe8572004f668a86365e4b1b
-    },
-    "Lectures": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "SortKeyName": "string",
-      "SortKeyType": "string",
-      "StreamArn": "string"
-    },
     "fcjvideoplatform": {
       "BucketName": "string",
+      "Region": "string"
+    },
+    "Course": {
+      "Name": "string",
+      "Arn": "string",
+      "StreamArn": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string"
+    },
+    "Lectures": {
+      "Name": "string",
+      "Arn": "string",
+      "StreamArn": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "Region": "string"
+    },
+    "Section": {
+      "Name": "string",
+      "Arn": "string",
+      "StreamArn": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
       "Region": "string"
     }
   }
